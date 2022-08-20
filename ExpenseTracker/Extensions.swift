cb1d5006b21e63c4c0d22461b0f4a65ba5d293de
dong2810 <dongvip99@gmail.com>
2022-08-20 15:27:49 +0700

    add color

---
+++ ExpenseTracker/Extensions.swift
@@ -0,0 +1,17 @@
+//
+//  Extensions.swift
+//  ExpenseTracker
+//
+//  Created by đông on 20/08/2022.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+	static let background = Color("Background")
+	static let icon = Color("Icon")
+	static let text = Color("Text")
+	static let systemBackground = Color(uiColor: .systemBackground)
+}
+
